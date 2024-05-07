5b0651a19d8d0f062269e7506e6b2c3bc12278b4
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-07 11:47:27 +0900

    storyboard 삭제 및 SceneDelegate에서 시작 VC 설정

--- BookSearchApp/BookSearchApp/SceneDelegate.swift
+++ BookSearchApp/BookSearchApp/SceneDelegate.swift
@@ -13,10 +13,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController() // 원하는 뷰컨트롤러로 변경해주기
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
