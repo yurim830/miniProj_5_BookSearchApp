04e526aa93e2697d344abf061dbea0ced5658119
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-07 19:44:22 +0900

    #1 - 책 상세 화면 VC 생성

---
+++ BookSearchApp/BookSearchApp/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  BookSearchApp
+//
+//  Created by 유림 on 5/7/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
