fc2195a7382123350a71edb09cff8d504eccbc30
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-10 16:52:26 +0900

    주석 수정

--- BookSearchApp/BookSearchApp/Model/NotificationName.swift
+++ BookSearchApp/BookSearchApp/Model/NotificationName.swift
@@ -9,10 +9,9 @@ import UIKit
 extension Notification.Name {
     static let presentedDetailView = Notification.Name("presentedDetailView")
     /* presentedDetailView 노티피케이션을 받으면
-     - TenRecentBooks 업데이트
      - SearchResultCollectionViewHeader의 collectionView.reloadData()
      
-     노티피케이션은 DetailViewController의 viewWillAppear에서 post
+     노티피케이션은 DetailViewController의 viewWillDisappear에서 post
      */
     
 }
